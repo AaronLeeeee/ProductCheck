e7558dfa7d68bb582cd58f3213479165b90675c3
苏晟 <neday@bk.ru>
2018-04-29 15:37:17 +0800

    接口变动需求

--- app/src/main/java/com/check/gf/gfapplication/CustomApplication.java
+++ app/src/main/java/com/check/gf/gfapplication/CustomApplication.java
@@ -1,9 +1,11 @@
 package com.check.gf.gfapplication;
 
 
+import android.app.Application;
 import android.content.Intent;
 import android.os.Environment;
 
+import com.blankj.utilcode.util.Utils;
 import com.check.gf.gfapplication.activity.LoginActivity;
 import com.check.gf.gfapplication.config.StaticConfig;
 import com.check.gf.gfapplication.helper.SharedPreferencesHelper;
@@ -12,8 +14,6 @@ import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
 import com.tencent.bugly.Bugly;
 import com.tencent.bugly.beta.Beta;
-
-import org.litepal.LitePalApplication;
 
 /**
  * 　　　　　　　　┏┓　　　┏┓+ +
@@ -41,7 +41,7 @@ import org.litepal.LitePalApplication;
  *
  * @author nEdAy
  */
-public class CustomApplication extends LitePalApplication {
+public class CustomApplication extends Application {
     // 对外提供整个应用生命周期的Context
     private static CustomApplication mInstance;
     private SharedPreferencesHelper sharedPreferencesHelper;
@@ -67,6 +67,8 @@ public class CustomApplication extends LitePalApplication {
 //        LeakCanary.install(this);
         // Normal app init code...
         mInstance = this;
+        // init it in the function of onCreate in ur Application
+        Utils.init(this);
         if (BuildConfig.LOG_DEBUG) {
             Logger.addLogAdapter(new AndroidLogAdapter());
             initBugly(true);
@@ -85,53 +87,53 @@ public class CustomApplication extends LitePalApplication {
      * @param logDebug 是否开启log
      */
     private void initBugly(boolean logDebug) {
-        /**
-         * true表示app启动自动初始化升级模块;
-         * false不会自动初始化;
-         * 开发者如果担心sdk初始化影响app启动速度，可以设置为false，
-         * 在后面某个时刻手动调用Beta.init(getApplicationContext(),false);
+        /*
+          true表示app启动自动初始化升级模块;
+          false不会自动初始化;
+          开发者如果担心sdk初始化影响app启动速度，可以设置为false，
+          在后面某个时刻手动调用Beta.init(getApplicationContext(),false);
          */
         Beta.autoInit = true;
-        /**
-         * true表示初始化时自动检查升级;
-         * false表示不会自动检查升级,需要手动调用Beta.checkUpgrade()方法;
+        /*
+          true表示初始化时自动检查升级;
+          false表示不会自动检查升级,需要手动调用Beta.checkUpgrade()方法;
          */
         Beta.autoCheckUpgrade = true;
-        /**
-         * 设置升级检查周期为60s(默认检查周期为0s)，60s内SDK不重复向后台请求策略);
+        /*
+          设置升级检查周期为60s(默认检查周期为0s)，60s内SDK不重复向后台请求策略);
          */
         Beta.upgradeCheckPeriod = 60 * 1000;
-        /**
-         * 设置启动延时为1s（默认延时3s），APP启动1s后初始化SDK，避免影响APP启动速度;
+        /*
+          设置启动延时为1s（默认延时3s），APP启动1s后初始化SDK，避免影响APP启动速度;
          */
         Beta.initDelay = 5 * 1000;
-        /**
-         * 设置通知栏大图标，largeIconId为项目中的图片资源;
+        /*
+          设置通知栏大图标，largeIconId为项目中的图片资源;
          */
         Beta.largeIconId = R.mipmap.ic_launcher;
-        /**
-         * 设置状态栏小图标，smallIconId为项目中的图片资源Id;
+        /*
+          设置状态栏小图标，smallIconId为项目中的图片资源Id;
          */
         Beta.smallIconId = R.mipmap.ic_launcher;
-        /**
-         * 设置更新弹窗默认展示的banner，defaultBannerId为项目中的图片资源Id;
-         * 当后台配置的banner拉取失败时显示此banner，默认不设置则展示“loading“;
+        /*
+          设置更新弹窗默认展示的banner，defaultBannerId为项目中的图片资源Id;
+          当后台配置的banner拉取失败时显示此banner，默认不设置则展示“loading“;
          */
-        //Beta.defaultBannerId = R.drawable.ic_update_banner;
-        /**
-         * 设置sd卡的Download为更新资源保存目录;
-         * 后续更新资源会保存在此目录，需要在manifest中添加WRITE_EXTERNAL_STORAGE权限;
+        // Beta.defaultBannerId = R.drawable.ic_update_banner;
+        /*
+          设置sd卡的Download为更新资源保存目录;
+          后续更新资源会保存在此目录，需要在manifest中添加WRITE_EXTERNAL_STORAGE权限;
          */
         Beta.storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-        /**
-         * 点击过确认的弹窗在APP下次启动自动检查更新时会再次显示;
+        /*
+          点击过确认的弹窗在APP下次启动自动检查更新时会再次显示;
          */
         Beta.showInterruptedStrategy = true;
-        /**
-         * 只允许在MainActivity上显示更新弹窗，其他activity上不显示弹窗;
-         * 不设置会默认所有activity都可以显示弹窗;
+        /*
+          只允许在MainActivity上显示更新弹窗，其他activity上不显示弹窗;
+          不设置会默认所有activity都可以显示弹窗;
          */
-        Beta.canShowUpgradeActs.add(LoginActivity.class);
+        //Beta.canShowUpgradeActs.add(SearchActivity.class);
         //统一初始化Bugly产品，包含Beta
         Bugly.init(this, StaticConfig.BUGLY_APP_ID, logDebug);//测试阶段建议设置成true，发布时设置为false。
     }
